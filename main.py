a54a152c0be2024dc83450347642651ce975ab0a
Lucas Guimaraes <lucasguimaraesdev@gmail.com>
2022-04-12 15:41:10 -0300

    alterações na classe calculadora.py e em main.py para dar privacidade aos atributos, sendo possível apenas alterar os valores dentro da classe e não no main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ comodo = Comodo(
     float(input('Qual a profundidade do comodo\n'))
 )
 
-print(f"A área das paredes é: {calc.calcular_area_paredes(comodo.largura, comodo.profundidade, comodo.altura)}")
+print(f"A área das paredes é: {calc.calcular_area_paredes(comodo)}")
 
-print(f"A área do teto é: {calc.calcular_area_teto(comodo.largura, comodo.profundidade)}")
+print(f"A área do teto é: {calc.calcular_area_teto(comodo)}")
 
 print(f"Serão necessários {calc.calcular_volume_tintas()} litros de tinta para pintar esse cômodo.")
